7b127a13ab064fa66af9e1c7fbe41c632dd22115
Luis E Lozano <lozano_22_lalo@yahoo.com>
2021-02-11 00:52:25 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,5 +1,8 @@
-def using_concat (my_favorite_things = ["raindrops on roses", "whiskers on kittens"], more_favs = ["sports cars", "flatiron school"])
-  my_favorite_things.concat(more_favs)
+require "pry"
+
+def using_concat (array, element)
+  binding.pry
+ # my_favorite_things.concat()
 end
 
 def using_insert(list_of_programming_languages = ["Ruby", "JavaScript", "Java", "C#", "Objective C", "C++", "PHP"], another_language = 'Python')
