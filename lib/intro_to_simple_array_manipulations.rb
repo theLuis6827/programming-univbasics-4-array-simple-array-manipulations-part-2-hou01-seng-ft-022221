ea3ad0940eb25a55ef1eaf7f713706afe3710bdb
Luis E Lozano <lozano_22_lalo@yahoo.com>
2021-02-11 00:22:10 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -5,13 +5,7 @@ def using_flatten(instruments = ["Saxophone", ["Piano", "Trumpet"], "Violin", "D
   instruments.flatten
 end
 
-def using_delete(instructors = ["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"])
-  no_offense_steven = instructors.delete("Steven")
-end
-
-def using_delete_at (famous_robots = ["Johnny 5", "R2D2", "Robocop"])
-  famous_robots.delete_at
-enddef using_concat (my_favorite_things = ["raindrops on roses", "whiskers on kittens"], more_favs = ["sports cars", "flatiron school"])
+def using_concat (my_favorite_things = ["raindrops on roses", "whiskers on kittens"], more_favs = ["sports cars", "flatiron school"])
   my_favorite_things.concat(more_favs)
 end
 
@@ -20,3 +14,10 @@ def using_insert(list_of_programming_languages = ["Ruby", "JavaScript", "Java", 
   list_of_programming_languages.insert("Python")
 end
 
+def using_delete(instructors = ["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"])
+  no_offense_steven = instructors.delete("Steven")
+end
+
+def using_delete_at (famous_robots = ["Johnny 5", "R2D2", "Robocop"])
+  famous_robots.delete_at
+end
