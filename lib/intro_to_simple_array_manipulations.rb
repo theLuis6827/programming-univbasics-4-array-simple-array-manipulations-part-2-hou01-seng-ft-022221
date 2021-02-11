2fd53d4c972ed8752697436e8a487fd92c2e1ddd
Luis E Lozano <lozano_22_lalo@yahoo.com>
2021-02-11 00:23:00 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,17 +1,17 @@
 def using_uniq (haircuts = ["Pixie", "Bob", "Mohawk", "Crew Cut", "Linka", "Wheeler", "Bob"])  haircuts.uniq
 end
 
+def using_insert(list_of_programming_languages = ["Ruby", "JavaScript", "Java", "C#", "Objective C", "C++", "PHP"], "Python")
+  new_array = []
+  list_of_programming_languages.insert("Python")
+end
+
 def using_flatten(instruments = ["Saxophone", ["Piano", "Trumpet"], "Violin", "Drums", "Flute"])
   instruments.flatten
 end
 
 def using_concat (my_favorite_things = ["raindrops on roses", "whiskers on kittens"], more_favs = ["sports cars", "flatiron school"])
   my_favorite_things.concat(more_favs)
-end
-
-def using_insert(list_of_programming_languages = ["Ruby", "JavaScript", "Java", "C#", "Objective C", "C++", "PHP"], "Python")
-  new_array = []
-  list_of_programming_languages.insert("Python")
 end
 
 def using_delete(instructors = ["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"])
